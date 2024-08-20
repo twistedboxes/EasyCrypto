397eddcbab95c7b67b3a1a4a87edf1c778c6f4f0
twistedboxes <tamponiandrea@gmail.com>
2024-08-20 12:21:29 +0200

    small fixes

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -14,7 +14,7 @@ const CardContainer = () => {
       <h2 className="text-3xl font-bold  text-center">
         Top {`${data.length}`} Tokens
       </h2>
-      <div className=" viewer my-8 p-2 w-screen h-[50vh] flex-wrap  max-w-1/2 justify-around flex gap-8">
+      <div className="viewer my-8 p-2 w-screen h-[50vh] flex-wrap  max-w-1/2 justify-evenly flex gap-8">
         {allCards.length === 0 ? (
           <p className="text-center font-bold text-red-500 text-xl">
             No tokens found
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
-
 const Header = () => {
   return (
-    <div className="flex h-[30vh] justify-center items-center flex-col ">
-        <h1 className="text-6xl leading-[1.5em] font-bold">Easy Crypto</h1>
-      <p className="px-4 text-center text-xl">Take a quick look at the top tokens!</p>
+    <div className=" cursor-pointer flex h-[30vh] justify-center items-center flex-col ">
+      <h1 className="text-6xl leading-[1.5em] font-bold">Easy Crypto</h1>
+      <p className="px-4 text-center text-xl">
+        Take a quick look at the top tokens!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
