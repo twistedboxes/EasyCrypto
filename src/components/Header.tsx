ea4012b363962832457213088592f97981159013
twistedboxes <tamponiandrea@gmail.com>
2024-08-21 11:26:44 +0200

    add mt-1 to subtext

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
       <h1 className="text-6xl text-center leading-[1.5em] sm:leading-[1.3] font-bold">
         Easy Crypto Viewer
       </h1>
-      <p className="px-4 text-center text-xl">
+      <p className="px-4 mt-1 text-center text-xl">
         Take a quick look at the top tokens!
       </p>
     </div>
